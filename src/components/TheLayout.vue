7f6c3ebace0307fc430fafeb1fb9191679e97c40
AnastasiaGolovanich <87997823+AnastasiaGolovanich@users.noreply.github.com>
2021-12-12 20:17:39 +0200

    Lesson 9: change table in Kanban page. Fix updated() hook in Tasks page

--- src/components/TheLayout.vue
+++ src/components/TheLayout.vue
@@ -3,26 +3,71 @@ div(class="main-page")
   sideBar(:notification="notification" @index-img="changeNotification($event)")
   section(class="right-block" id="right-block")
     Header
-    router-view(@index-img="changeNotification($event)")/
+    router-view(@index-img="changeNotification($event)" @sync-tasks="sync" :tasks="tasks")/
 </template>
 <script lang="ts">
 import sideBar from '@/components/TheSideBar.vue'
 import Header from '@/components/TheHeader.vue'
 import { defineComponent } from 'vue'
-
+enum Status {
+  todo = 'todo',
+  inprogress = 'inprogress',
+  done = 'done'
+}
+interface Tasks {
+  id: number
+  name: string
+  description: string
+  date: string
+  delay: string
+  status: Status
+}
 export default defineComponent({
   components: {
     sideBar,
     Header
   },
-  data () {
+  data: function () {
     return {
-      notification: 3
+      notification: 3,
+      tasks: [] as Tasks[]
     }
+  },
+  created () {
+    this.tasks = [
+      {
+        id: 1,
+        name: 'Install programs',
+        description: 'Install Node.js and Vue CLI on PC',
+        date: '12/31/2021',
+        delay: 'animation-delay:0s',
+        status: 'done'
+      },
+      {
+        id: 2,
+        name: 'Read the theory',
+        description: 'Working with forms',
+        delay: 'animation-delay:1s',
+        date: '12/31/2021',
+        status: 'todo'
+      },
+      {
+        id: 3,
+        name: 'Practice',
+        description: 'On the Tasks tab, create a form to add a new task. The form must contain 2 fields: title and description of the task.',
+        delay: 'animation-delay:2s',
+        date: '12/31/2021',
+        status: 'todo'
+      }
+    ] as Tasks[]
+    this.$emit('sync-tasks', this.tasks)
   },
   methods: {
     changeNotification (e: number) : void {
       this.notification = e
+    },
+    sync (data: any) {
+      this.tasks = data
     }
   }
 })
