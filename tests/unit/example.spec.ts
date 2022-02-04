ac7002cde497ae77daef9ddfc80d39c4485001fe
AnastasiaGolovanich <anastasiakukharenko777@gmail.com>
2022-02-04 18:54:19 +0200

    Lesson 15: fix

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,13 @@
 import { shallowMount } from '@vue/test-utils'
-import Tasks from '@/views/Tasks.vue'
+import NotFound from '@/views/NotFound.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(Tasks, {
+    const wrapper = shallowMount(NotFound, {
       props: { msg }
     })
-    expect(wrapper.vm.$data.isModalVisible).toBe(true)
+    // expect(wrapper.vm.$data.isModalVisible).toBe(true)
+    expect(wrapper.element).toMatchSnapshot()
   })
 })
